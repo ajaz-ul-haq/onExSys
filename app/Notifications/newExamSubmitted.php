a9fe3ce00987a752dd669b75f8e3a5dc379bc8c7
Ajaz Ul Haq <ajaz.ul.haq@ladybirdweb.com>
2022-03-17 13:15:47 +0530

    In app Notification for new exam creation and Exam submission

---
+++ app/Notifications/newExamSubmitted.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class newExamSubmitted extends Notification
+{
+    use Queueable;
+    public $subject, $name,$class;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($name,$subject,$class)
+    {
+        $this->name= $name;
+        $this->subject = $subject;
+        $this->class = $class;
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'data'=>$this->name.' class ('.$this->class.') Submitted exam of '.$this->subject,
+        ];
+    }
+}
